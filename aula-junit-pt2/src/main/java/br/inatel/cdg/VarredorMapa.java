b0e553971aeaa32567f57aee066c9297dbddd7b7
phillima <phyllipe_slf@yahoo.com.br>
2020-10-26 21:03:03 -0300

    Aula-Junit-PT2

---
+++ aula-junit-pt2/src/main/java/br/inatel/cdg/VarredorMapa.java
@@ -0,0 +1,28 @@
+package br.inatel.cdg;
+
+import java.util.HashMap;
+
+public class VarredorMapa {
+	
+	public void varreAsync(Resultado res) {
+		
+		new Thread(() -> {
+			boolean ret = varreSync();
+			res.setResultado(ret);
+		}).start(); 
+	}
+	
+	public boolean varreSync() {
+		HashMap<Integer, Integer> mapa = new HashMap<Integer, Integer>();
+		
+		for (int i = 0; i < 1000; i++) {
+			mapa.put(i, i);
+		}
+		
+		mapa.forEach((k,v)->{
+			mapa.get(k);
+		});
+		return true;
+	}
+	
+}
